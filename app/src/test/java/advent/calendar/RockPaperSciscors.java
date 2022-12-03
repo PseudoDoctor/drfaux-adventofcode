74115e0485d2256d5cd869a4677c9e1fd614e258
Pseudo Doctor <onineko@gmail.com>
2022-12-02 18:05:25 -0700

    Day 2 started

---
+++ app/src/test/java/advent/calendar/RockPaperSciscors.java
@@ -0,0 +1,20 @@
+package advent.calendar;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class RockPaperSciscors {
+
+}
+enum RPS {
+    String s;
+    ROCK("A"),
+    PAPER("B"),
+    SCISCORS("C"),
+    ;
+    RPS(String s){
+        this.s = s;
+    }
+
+}
