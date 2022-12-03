e11ed502eddc8764dbb07a1343991af17539e582
Pseudo Doctor AKA Dr Faux <onineko@gmail.com>
2022-12-03 13:01:09 -0700

    Fix Day 1 Manual Java

---
+++ java-based/src/main/java/advent/calendar/App.java
@@ -0,0 +1,79 @@
+package advent.calendar;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.stream.Collectors;
+import advent.calendar.day1.Day1Manual;
+
+public class App {
+
+    public static final String ROOT_DIR = "D:/git/pseudodoctor-advent-2022/";
+    // public static final String DAY1_INPUT = "day1/input.omcsettings.txt";
+    public static final String DAY1_INPUT = "day1/smallinput.txt";
+    
+
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public BufferedReader getResourceAsBufferedReader(String path) {
+        InputStream is = getClass().getClassLoader().getResourceAsStream(path);
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        return br;
+    }
+    public BufferedReader getBufferedReader(String path) throws FileNotFoundException {
+        return new BufferedReader(new FileReader(path));
+    }
+
+    public void day1(){
+        try (BufferedReader br = new App().getBufferedReader(ROOT_DIR+DAY1_INPUT)){
+            ArrayList<Elf> elves = new Elf().populateElves(br);
+            // Day 1 part 1, max elf
+            int max = new Elf().maxCalories(elves);
+            System.out.println("The most calories carried by a single elf is "+max);
+            // Day 1 part 2, top 3 elf total
+            Map<Integer,Elf> elfMap = new Elf().sortedElves(elves);
+            int elfCount = elfMap.size();
+            Map<Integer,Elf> top3ElfMap = elfMap.entrySet().stream()
+                .skip(elfCount-3)
+                .collect(Collectors.toMap(
+                    Map.Entry::getKey
+                   , Map.Entry::getValue
+                    ));
+            top3ElfMap.forEach((key,val)->{
+                    out("Top Elf #"+val.getOriginalIndex()+" had "+val.getTotalCalories()+" calories");
+                });
+            int topElfTotal = new Elf().grandTotal(top3ElfMap);
+            System.out.println("Total for top 3 elfs "+topElfTotal);
+            
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    public void day2() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(new App().getGreeting());
+        new App().day1();
+        Day1Manual.day1Part1();
+        Day1Manual.day1Part2();
+        // new App().day2();
+        
+    }
+
+    private void out(String s){
+        // System.out.println(s);
+    }
+    
+}
