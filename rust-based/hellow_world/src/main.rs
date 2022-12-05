21ae77ebb78a219d0fa80446c587e5895a043c87
Pseudo Doctor <onineko@gmail.com>
2022-12-05 11:17:57 -0700

    switch to windows

--- rust-based/hellow_world/src/main.rs
+++ rust-based/hellow_world/src/main.rs
@@ -5,27 +5,6 @@ use std::{
     io::{BufRead, BufReader, Read},
     str,
 };
-static ROCK: i32 = 1;
-static PAPER: i32 = 2;
-static SCISSORS: i32 = 3;
-static LOSE: i32 = 0;
-static DRAW: i32 = 3;
-static WIN: i32 = 6;
-
-// Rock defeats Scissors,
-// Scissors defeats Paper, and
-// Paper defeats Rock.
-// If both players choose the same shape, the round instead ends in a draw.
-// The score for a single round is the score for the shape you selected
-// (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the
-// outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
-// A Y
-// B X
-// C Z
-// Round 1: Rock A, choose Paper Z. You win, score won 6 + choose paper 2 = 8.
-// Round 2: Paper B, choose Rock X. You lose, score lose 0 + choose rock 1 = 1.
-// Round 3: SCISSORS C, choose SCISSORS Z. Draw, score draw 2 + choose SCISSORS 3 = 6
-
 pub fn exists(path: &str) -> bool {
     if metadata(path).is_ok() {
         let md = metadata(path).unwrap();
@@ -50,106 +29,45 @@ fn dump_file(path: &str) -> Vec<u8> {
 }
 fn main() {
     println!("Hello, world!");
-    let _small_input = "day2/smallinput.txt";
-    let _regular_input = "day2/input.txt";
-    // println!("{}", exists(_regular_input));
+    let _small_input = "day3/smallinput.txt";
+    let _regular_input = "day3/input.txt";
+    println!("{}", exists(_small_input));
     let data = dump_file(_small_input);
-    let score = rock_paper_scisors(data);
-    println!("total score {}",score)
-}
-fn rock_paper_scisors(_data: Vec<u8>) -> i32 {
-    let mut rounds: Vec<i32> = Vec::new();
-    let mut total: i32 = 0;
-    let mut prev: u8 = b'0';
-    let mut play: u8 = b'0';
-    let mut response: u8 = b'0';
-    for b in _data {
-        print!("{}-", b);
-        if b == b' ' {
-            play = prev;
-        } else if b == b'\n' {
-            response = prev;
-            let score = decidepart2(play, response);
-            rounds.push(score);
-            total += score;
+    let mut buffer: Vec<char> = Vec::new();
+    let mut comparts: Vec<i32> = Vec::new();
+    for b in data {
+        if b == b'\n' {
+            comparts.push(compart_priority(buffer.clone()));
+            buffer.clear();
+        } else {
+            buffer.push(b as char);
         }
-        prev = b;
-
     }
-    for r in rounds {
-            println!(
-                "Rounds {}",r
-            )
+    for c in comparts {
+        println!("Compart {}", c);
     }
-    println!("Total {}",total);
-    total
 }
 
-fn decidepart1(_play: u8, _response: u8) -> i32 {
-        // match b {
-        //     b'A'|b'X' => print!("Rock"),
-        //     b'B'|b'Y' => print!("Paper"),
-        //     b'C'|b'Z' => print!("Scis"),
-        //     b' ' => print!(" "),
-        //     b'\n'=> println!(""),
-        //     _ => println!("Unhandled")
-        // }
-
-    let mut score: i32 = 0;
-    match _play {
-        b'A' => {print!("Rock vs ");
-            match _response {
-            b'X' => {print!("Rock");score += DRAW + ROCK},
-            b'Y' => {print!("Paper");score += WIN  + PAPER},
-            b'Z' => {print!("Sciscors");score += LOSE + SCISSORS},
-            _ => println!("Unhandled")
-        }},
-        b'B' => {print!("Paper vs ");
-             match _response {
-            b'X' => {print!("Rock");score += LOSE + ROCK},
-            b'Y' => {print!("Paper");score += DRAW + PAPER},
-            b'Z' => {print!("Sciscors");score += WIN  + SCISSORS},
-            _ => println!("Unhandled")
-        }},
-        b'C' => {print!("Sciscors vs ");
-            match _response {
-            b'X' => {print!("Rock");score += WIN + ROCK},
-            b'Y' => {print!("Paper");score += LOSE + PAPER},
-            b'Z' => {print!("Sciscors");score += DRAW + SCISSORS},
-            _ => println!("Unhandled")
-        }},
-        _ => println!("Unhandled")
+fn compart_priority(_buffer: Vec<char>) -> i32 {
+    let mut p: i32 = -2;
+    for c in _buffer {
+        println!("Asking about {} and got {}", c, prioity(c))
     }
-    println!(" {}",score);
-    score
+    p
 }
 
-fn decidepart2(_play: u8, _response: u8) -> i32 {
-    let mut score: i32 = 0;
-    match _play {
-        b'A' => {print!("Rock vs ");
-            match _response {
-            b'X' => {print!(" LOSE Sciscors");score += LOSE + SCISSORS},
-            b'Y' => {print!(" DRAW Rock");score += DRAW  + ROCK},
-            b'Z' => {print!(" WIN Paper");score += WIN + PAPER},
-            _ => println!("Unhandled")
-        }},
-        b'B' => {print!("Paper vs ");
-             match _response {
-            b'X' => {print!(" LOSE Rock");score += LOSE + ROCK},
-            b'Y' => {print!(" DRAW Paper");score += DRAW + PAPER},
-            b'Z' => {print!(" WIN Sciscors");score += WIN  + SCISSORS},
-            _ => println!("Unhandled")
-        }},
-        b'C' => {print!("Sciscors vs ");
-            match _response {
-            b'X' => {print!(" LOSE Paper");score += LOSE + PAPER},
-            b'Y' => {print!(" DRAW Sciscosrs");score += DRAW + SCISSORS},
-            b'Z' => {print!(" WIN Rock");score += WIN + ROCK},
-            _ => println!("Unhandled")
-        }},
-        _ => println!("Unhandled")
+fn prioity(_char: char) -> i32 {
+    // Lowercase item types a through z have priorities 1 through 26.
+    // Uppercase item types A through Z have priorities 27 through 52.
+    let mut p: i32 = -2;
+    match _char as u8 {
+        b'a'..=b'z' => p = ((_char as u8) - b'a' + 1) as i32,
+        b'A'..=b'Z' => p = ((_char as u8) - b'A' + 27) as i32,
+        _ => {
+            println!("Unhandled: {}", _char);
+            p = -1
+        }
     }
-    println!(" {}",score);
-    score
+    println!("Char {} has priority {}", _char, p);
+    p
 }
