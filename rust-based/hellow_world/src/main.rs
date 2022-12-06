33fc136ab68a25a754dfb46e2dc007aeb305a5fb
Pseudo Doctor AKA Dr Faux <onineko@gmail.com>
2022-12-06 00:40:16 -0700

    Day 3 part 1 and 2 rewritten and complete

--- rust-based/hellow_world/src/main.rs
+++ rust-based/hellow_world/src/main.rs
@@ -5,16 +5,6 @@ use std::{
     io::{BufRead, BufReader, Read},
     str,
 };
-pub fn exists(path: &str) -> bool {
-    if metadata(path).is_ok() {
-        let md = metadata(path).unwrap();
-        println!("is dir: {}", md.is_dir());
-        println!("is file: {}", md.is_file());
-        return true;
-    }
-    false
-}
-
 fn dump_file(path: &str) -> Vec<u8> {
     let mut file = File::open(path).unwrap();
     // let mut contents = String::new();
@@ -31,68 +21,105 @@ fn main() {
     println!("Hello, world!");
     let _small_input = "day3/smallinput.txt";
     let _regular_input = "day3/input.txt";
-    println!("{}", exists(_small_input));
+    let _tiny_input = "day3/tinyinput.txt";
+    // println!("{}", exists(_tiny_input));
     let data = dump_file(_regular_input);
-    let mut buffer: Vec<char> = Vec::new();
-    let mut single_priorities: Vec<i32> = Vec::new();
+    let mut elfs: Vec<String> = Vec::new();
+    let mut buffer: Vec<u8> = Vec::new();
     for b in data {
         if b == b'\n' {
-            single_priorities.push(compart_priority(buffer.clone()));
+            elfs.push(buffer_2_string(buffer.clone()));
             buffer.clear();
         } else {
-            buffer.push(b as char);
+            buffer.push(b);
         }
     }
-    let mut sum: i32 = 0;
-    for c in single_priorities {
-        sum += c;
+    // part 1
+    let mut sum1: i32 = 0;
+    for elf in elfs.clone() {
+        let len = elf.len();
+        let lenhalf = len/2;
+        let left = String::from(&elf[..lenhalf]);
+        let right = String::from(&elf[lenhalf..]);
+        // println!("left {} right {}",left,right);
+        // let common = common_characters(left.clone(),right.clone());
+        let common_single = common_character_single(left.clone(),right.clone());
+        // println!("Common character for {} and {} is {}",left,right,common_single.clone());
+        sum1 += get_priority(common_single);
     }
-    println!("Sum {}",sum);
+    println!("Sum1 {}",sum1);
+    // part 2
+    let mut sum2: i32 = 0;
+    loop {
+        if elfs.len() >= 3 {
+            let one = elfs.pop().unwrap();
+            let two = elfs.pop().unwrap();
+            let three = elfs.pop().unwrap();
+            let common_onetwo = common_characters(one.clone(), two);
+            let common_onetwothree = common_characters(common_onetwo, three);
+            let common = common_character_single(one.clone(), common_onetwothree.clone());
+            // println!( "Common char {}",common)
+            sum2 += get_priority(common);
+        } else {
+            if elfs.len() == 0 {
+                break;
+            } else {
+                println!("Bad state ");
+                break;
+            }
+        }
+    }
+    println!("Sum2 {}",sum2);
+    
 }
 
-fn compart_priority(_buffer: Vec<char>) -> i32 {
-    let mut output: String = String::from("Compartments '");
-    let mut _p: i32 = -2;
-    let len = _buffer.len();
-    let newlen = len/2;
-    let left: Vec<char> = _buffer[..newlen].to_vec();
-    for c in &left{
-        output += &String::from(*c);
+fn buffer_2_string(_buffer: Vec<u8>) -> String {
+    let mut s = String::from("");
+    for b in _buffer {
+        s.push(b as char);
     }
-    output += "' and '";
-    let right: Vec<char> = _buffer[newlen..].to_vec();
-    for c in &right{
-        output += &String::from(*c);
+    s
+}
+
+fn common_characters(_a: String,_b:String)->String{
+    let mut _s = String::from("");
+    for (i,a) in _a.bytes().enumerate() {
+        if _b.contains(a as char) {
+            _s.push(a as char);
+            // println!("{} from left at index {}",a as char,i);
+        }
     }
-    let m = get_matching(left,right);
-    _p = get_priority(m);
-    println!("{}' have '{}' in common with priority '{}'",output,m,_p);
-    _p
+    for (i,b) in _b.bytes().enumerate(){
+        if _s.contains(b as char) {
+            // println!("{} from right at index {}",b as char, i);
+        }
+    }
+    _s
+}
+
+fn common_character_single(_left: String,_right: String) -> char{
+    for l in _left.chars() {
+        for r in _right.chars() {
+            if l == r { return l}
+        }
+    }
+    '-'
 }
 
 fn get_priority(_char: char) -> i32 {
     // Lowercase item types a through z have priorities 1 through 26.
     // Uppercase item types A through Z have priorities 27 through 52.
-    let mut p: i32 = -2;
+    let mut _p: i32 = -2;
     match _char as u8 {
         // range a-z and A-Z
         // take the char's u8 value and subtract b'a' from it, then add 1 or 27
-        b'a'..=b'z' => p = ((_char as u8) - b'a' + 1) as i32,
-        b'A'..=b'Z' => p = ((_char as u8) - b'A' + 27) as i32,
+        b'a'..=b'z' => _p = ((_char as u8) - b'a' + 1) as i32,
+        b'A'..=b'Z' => _p = ((_char as u8) - b'A' + 27) as i32,
         _ => {
             println!("Unhandled: {}", _char);
-            p = -1
+            _p = -1
         }
     }
     // println!("Char {} has priority {}", _char, p);
-    p
-}
-
-fn get_matching(_left: Vec<char>,_right: Vec<char>) -> char{
-    for l in &_left {
-        for r in &_right {
-            if l == r { return *l}
-        }
-    }
-    '-'
+    _p
 }
