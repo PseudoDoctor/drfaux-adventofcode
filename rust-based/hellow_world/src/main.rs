d9cafaa6d83cbafa6dbfc2f443bf086aa96db8fa
Pseudo Doctor AKA Dr Faux <onineko@gmail.com>
2022-12-09 15:01:41 -0700

    Day 6 Part 2

--- rust-based/hellow_world/src/main.rs
+++ rust-based/hellow_world/src/main.rs
@@ -30,7 +30,7 @@ fn main() {
     println!("Hello, world!");
     let _small_input = "day6/smallinput.txt";
     let _regular_input = "day6/input.txt";
-    let data = dump_file(_small_input);
+    let data = dump_file(_regular_input);
     let mut line: Vec<u8> = Vec::new();
     for b in data {
         if b == b'\n' {
@@ -46,8 +46,33 @@ fn main() {
         }
     }
 }
-
 fn get_start_of_packet(_line: Vec<u8>) -> i32 {
+    // first time the last four characters were different
+    let mut four: Vec<u8> = Vec::new();
+    let mut match_count: i32 = 0;
+    for b in _line {
+        if four.len() < 4 {
+            four.push(b);
+        } else {
+            let four_chars = four.iter().map(|q| *q as char).collect::<Vec<_>>();
+            let four_string = String::from_iter(four_chars);
+            if (any_repeats(four.clone())){
+                four.reverse();
+                let _old = four.pop().unwrap() as char;
+                // println!("Removing {}",_old);
+                four.reverse();
+                four.push(b);
+            
+            }else {
+                // FOUND THE FIRST NON-REPEATING
+                return match_count;
+            }
+        }
+        match_count += 1;
+    }
+    -2
+}
+fn get_start_of_packet_deprecated(_line: Vec<u8>) -> i32 {
     // first time the last four characters were different
     let mut four_u8s: Vec<u8> = Vec::new();
     let mut match_count: i32 = -1;
@@ -88,41 +113,28 @@ fn get_start_of_message(_line: Vec<u8>) -> i32 {
     // first time the last fourteen characters were different
 
     let mut fourteen: Vec<u8> = Vec::new();
-    let mut match_count: i32 = -1;
+    let mut match_count: i32 = 0;
     for b in _line {
-        if fourteen.len() < 15 {
+        if fourteen.len() < 14 {
             fourteen.push(b);
         } else {
             let fourteen_chars = fourteen.iter().map(|q| *q as char).collect::<Vec<_>>();
             let fourteen_string = String::from_iter(fourteen_chars);
-            println!("Last fourteen {}",fourteen_string);
+            // println!("Last fourteen {} on count {}",fourteen_string,match_count);
             // If/Thens for four charactesr require 6 comparisons
-            if ( fourteen[0] == fourteen[1]
-                || fourteen[0] == fourteen[2]
-                || fourteen[0] == fourteen[3] 
-                // First  ^
-                // SEcond  v
-                || fourteen[1] == fourteen[2] 
-                || fourteen[1] == fourteen[3] 
-                // SEcond ^
-                // Third and fourth v
-                || fourteen[2] == fourteen[3] ) {
-                // One of the last four characters matched, delete the oldest and move on
+                // What about 14 characters? 105 comparisons?
+                // This needs a loop. A recursive loop?
+                // I don't think I can code this myself
+                // let's use the Contains function (if it exists)
+                
+            if (any_repeats(fourteen.clone())){
                 fourteen.reverse();
                 let _old = fourteen.pop().unwrap() as char;
                 // println!("Removing {}",_old);
                 fourteen.reverse();
                 fourteen.push(b);
-            } else if(
-                // What about 14 characters? 105 comparisons?
-                // This needs a loop. A recursive loop?
-                // I don't think I can code this myself
-                // let's use the Contains function (if it exists)
-                false
-            ) {
-
-            }
-            {
+            
+            } else {
                 // FOUND THE FIRST NON-REPEATING
                 return match_count;
             }
@@ -130,4 +142,31 @@ fn get_start_of_message(_line: Vec<u8>) -> i32 {
         match_count += 1;
     }
     -2
+}
+
+fn any_repeats(_subset:Vec<u8>)->bool{
+    let cs = _subset.iter().map(|q| *q as char).collect::<Vec<_>>();
+    let sg = String::from_iter(cs);
+    let mut sub_sg = String::from("");
+    // print!("repeats? {}  ",sg);
+    for (i,c) in sg.chars().enumerate(){
+        // print!(" is {} ",c);
+        // 1. Create a slice around index i as that char should not be counted
+        if(i==0){
+            sub_sg = format!("{}",&sg[1..]);
+        } else {
+            let left = &sg[0..i];
+            let right = &sg[(i+1)..];
+            sub_sg = format!("{}{}",left,right);
+        }
+        
+        // print!("in {}",sub_sg);
+        if sub_sg.contains(c) {
+            // println!(" yes");
+            return true
+        }
+        
+    }
+    println!(" none");
+    false
 }
