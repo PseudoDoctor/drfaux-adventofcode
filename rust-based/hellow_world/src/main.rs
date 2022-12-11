8b95bb044952ce3b0cc515dcdf474446b3c9cfd4
Pseudo Doctor AKA Dr Faux <onineko@gmail.com>
2022-12-10 21:24:07 -0700

    Day 7 Part 1 first pass incomplete

--- rust-based/hellow_world/src/main.rs
+++ rust-based/hellow_world/src/main.rs
@@ -26,147 +26,133 @@ fn dump_file(path: &str) -> Vec<u8> {
     data
 }
 
+#[derive(Debug, Clone)]
+struct CMD {
+    working: String,
+    command: String,
+}
+
+#[derive(Hash, PartialEq, Eq, Debug, Clone)]
+struct DirFil {
+    name: String,
+    parent: String,
+    size: i32,
+    child_dirs: Option<Vec<String>>
+}
+
 fn main() {
     println!("Hello, world!");
-    let _small_input = "day6/smallinput.txt";
-    let _regular_input = "day6/input.txt";
-    let data = dump_file(_regular_input);
+    let _small_input = "day7/smallinput.txt";
+    let _regular_input = "day7/input.txt";
+    let data = dump_file(_small_input);
+    let mut dirs: Vec<DirFil> = Vec::new();
+    dirs.push(DirFil{name:String::from("/"),parent:String::from("/"),child_dirs:None,size:-1});
+    let mut fils: Vec<DirFil> = Vec::new();
     let mut line: Vec<u8> = Vec::new();
+    let mut pwd: String = String::from("");
+    let mut dir_sizes: HashMap<DirFil, i32> = HashMap::new();
     for b in data {
         if b == b'\n' {
             let line_chars = line.iter().map(|q| *q as char).collect::<Vec<_>>();
-            let line_string = String::from_iter(line_chars);
+            let line_string = String::from_iter(line_chars.clone());
             // process line
-            let packet_marker: i32 = get_start_of_packet(line.clone());
-            let message_marker: i32 = get_start_of_message(line.clone());
-            println!("Marker Packet {} Message {} for {}",packet_marker, message_marker, line_string);
+            match line_chars[0] {
+                '$' => {
+                    print!("CMD");
+                    let cm = &line_string[2..];
+                    let cmd = CMD {
+                        working: pwd.clone(),
+                        command: cm.to_string(),
+                    };
+                    if cmd.command.contains("cd ") {
+                        let dest_dir = &cm[3..];
+                        println!("Change Directory to '{}'", dest_dir);
+                        if (dest_dir == "..") {
+                            // println!("Need to find parent directory of '{}'",pwd);
+                            // let dirs2 = dirs.clone();
+                            // let cwd = dirs2.into_iter().find(|x|x.name == pwd).unwrap();
+                            let cwd = get_current_working_directory(dirs.clone(), pwd);
+                            println!("Current Working Directory {:?}", cwd);
+                            pwd = cwd.parent;
+                        } else if dest_dir == "/" {
+                            println!("Changing to root");
+                            pwd = dest_dir.to_string();
+                        } else {
+                            pwd = dest_dir.to_string();
+                        }
+                    } else if cmd.command.contains("ls") {
+                        println!("Listing directory '{}'", pwd);
+                    }
+                }
+                'd' => {
+                    print!("DIR");
+                    let parent = pwd.clone();
+                    let tmp_name = &line_string[4..];
+                    let name = tmp_name.to_string();
+                    let dir = DirFil { name, parent, child_dirs: None, size: -1 };
+                    println!("Adding Dir {:?}", dir);
+                    dirs.push(dir);
+                }
+                '0'..='9' => {
+                    print!("FIL");
+                    let parent = pwd.clone();
+                    let re = Regex::new(r"(\d+) (.*)").unwrap();
+                    let mut size: i32 = 0;
+                    let mut name: String = String::from("");
+                    for cap in re.captures_iter(&line_string) {
+                        size = cap.get(1).unwrap().as_str().parse().unwrap();
+                        name = cap.get(2).unwrap().as_str().to_string();
+                    }
+                    let fil = DirFil { name, parent, size, child_dirs: None };
+                    println!("Adding File {:?}", fil);
+                    fils.push(fil);
+                }
+                _ => {
+                    print! {"ERR"}
+                }
+            }
+            // clear line
             line.clear();
         } else {
             line.push(b)
         }
     }
-}
-fn get_start_of_packet(_line: Vec<u8>) -> i32 {
-    // first time the last four characters were different
-    let mut four: Vec<u8> = Vec::new();
-    let mut match_count: i32 = 0;
-    for b in _line {
-        if four.len() < 4 {
-            four.push(b);
-        } else {
-            let four_chars = four.iter().map(|q| *q as char).collect::<Vec<_>>();
-            let four_string = String::from_iter(four_chars);
-            if (any_repeats(four.clone())){
-                four.reverse();
-                let _old = four.pop().unwrap() as char;
-                // println!("Removing {}",_old);
-                four.reverse();
-                four.push(b);
-            
-            }else {
-                // FOUND THE FIRST NON-REPEATING
-                return match_count;
+    // Finished parsing input
+    // Now calculate sizes
+    // Each dir's size, without regard for the presence of children
+    for dir in dirs.clone() {
+        let mut tmp_size: i32 = 0;
+        for fil in &fils {
+            if fil.parent == dir.name {
+                tmp_size += fil.size;
             }
         }
-        match_count += 1;
+        println!("Dir {:?} Size {}", dir, tmp_size);
+        dir_sizes.insert(dir, tmp_size);
+        // TODO: Add children dirs to parent dirs. Loop, find children? I dunno
     }
-    -2
-}
-fn get_start_of_packet_deprecated(_line: Vec<u8>) -> i32 {
-    // first time the last four characters were different
-    let mut four_u8s: Vec<u8> = Vec::new();
-    let mut match_count: i32 = -1;
-    for b in _line {
-        if four_u8s.len() < 5 {
-            four_u8s.push(b);
-        } else {
-            // do any of the four match?
-            // a match or switch wouldn't work...
-            // If/Thens should work... four charactesr require 6 comparisons
-            if ( four_u8s[0] == four_u8s[1]
-                || four_u8s[0] == four_u8s[2]
-                || four_u8s[0] == four_u8s[3] 
-                // First  ^
-                // SEcond  v
-                || four_u8s[1] == four_u8s[2] 
-                || four_u8s[1] == four_u8s[3] 
-                // SEcond ^
-                // Third and fourth v
-                || four_u8s[2] == four_u8s[3] ) {
-                // One of the last four characters matched, delete the oldest and move on
-                four_u8s.reverse();
-                let _old = four_u8s.pop().unwrap() as char;
-                // println!("Removing {}",_old);
-                four_u8s.reverse();
-                four_u8s.push(b);
-            } else {
-                // FOUND THE FIRST NON-REPEATING
-                return match_count;
-            }
+    let mut adjusted_dir_sizes: HashMap<DirFil, i32> = HashMap::new();
+    for dir_map in dir_sizes.clone() {
+        let child_dirs = child_dirs(dirs.clone(), dir_map.0);
+        if( child_dirs.len() > 0){
+            // TODO: How deep do we go?
         }
-        match_count += 1;
     }
-    -2
+    println!("Dirs sizes {:?}", dir_sizes.clone());
+    println!("");
 }
 
-fn get_start_of_message(_line: Vec<u8>) -> i32 {
-    // first time the last fourteen characters were different
-
-    let mut fourteen: Vec<u8> = Vec::new();
-    let mut match_count: i32 = 0;
-    for b in _line {
-        if fourteen.len() < 14 {
-            fourteen.push(b);
-        } else {
-            let fourteen_chars = fourteen.iter().map(|q| *q as char).collect::<Vec<_>>();
-            let fourteen_string = String::from_iter(fourteen_chars);
-            // println!("Last fourteen {} on count {}",fourteen_string,match_count);
-            // If/Thens for four charactesr require 6 comparisons
-                // What about 14 characters? 105 comparisons?
-                // This needs a loop. A recursive loop?
-                // I don't think I can code this myself
-                // let's use the Contains function (if it exists)
-                
-            if (any_repeats(fourteen.clone())){
-                fourteen.reverse();
-                let _old = fourteen.pop().unwrap() as char;
-                // println!("Removing {}",_old);
-                fourteen.reverse();
-                fourteen.push(b);
-            
-            } else {
-                // FOUND THE FIRST NON-REPEATING
-                return match_count;
-            }
-        }
-        match_count += 1;
-    }
-    -2
+fn get_current_working_directory(_dirs: Vec<DirFil>, _pwd: String) -> DirFil {
+    let cwd = _dirs.into_iter().find(|x| x.name == _pwd).unwrap();
+    cwd
 }
 
-fn any_repeats(_subset:Vec<u8>)->bool{
-    let cs = _subset.iter().map(|q| *q as char).collect::<Vec<_>>();
-    let sg = String::from_iter(cs);
-    let mut sub_sg = String::from("");
-    // print!("repeats? {}  ",sg);
-    for (i,c) in sg.chars().enumerate(){
-        // print!(" is {} ",c);
-        // 1. Create a slice around index i as that char should not be counted
-        if(i==0){
-            sub_sg = format!("{}",&sg[1..]);
-        } else {
-            let left = &sg[0..i];
-            let right = &sg[(i+1)..];
-            sub_sg = format!("{}{}",left,right);
+fn child_dirs(_dirs: Vec<DirFil>, _dir: DirFil) -> Vec<DirFil> {
+    let mut child_dirs:Vec<DirFil> = Vec::new();
+    for dir in _dirs {
+        if dir.parent == _dir.name {
+            child_dirs.push(dir);
         }
-        
-        // print!("in {}",sub_sg);
-        if sub_sg.contains(c) {
-            // println!(" yes");
-            return true
-        }
-        
     }
-    println!(" none");
-    false
+    child_dirs
 }
